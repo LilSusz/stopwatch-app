f93bc70b76ee87750fd302eb463c723c9e103063
Liliana Susz <proglilsu@gmail.com>
2023-07-12 19:14:37 +0200

    Add 'Container' and 'Button' components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import Time from './components/Time/Time.js';
+import { useState, useEffect } from 'react';
+import Container from './components/Container/Container';
+import Button from './components/Button/Button';
+
+
 
 const App = () => {
   const [time, setTime] = useState(0);
-  const [start, setStart] = useState(null);
+  const [timer, setTimer] = useState(null);
+
+  const start = () => {
+    setTimer(setInterval(() => {
+      setTime(prevValue => prevValue +1);
+    }, 1))
+  };
+
+  const stop = () => {
+    clearInterval(timer);
+    setTimer();
+  };
+
+  const reset = () => {
+    setTime(0);
+    setTimer(null);
+  }
 
   return (
-    <Time />
+    <Container>
+      <Button value={start}>start</Button>
+      <Button value={stop}>stop</Button>
+      <Button value={reset}>reset</Button>
+    </Container>
   );
-};
+}
 
 export default App;
